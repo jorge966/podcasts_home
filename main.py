8a53ca626b6674c7641752bf8c32e827bb76d8a5
jorge966 <martinezjorge966@live.com>
2025-01-21 18:48:34 -0500

    created the create_episode and create_podcast endpoint and tested with success

--- main.py
+++ main.py
@@ -46,22 +46,47 @@ def create_podcast():
     curr = conn.cursor()
     post_data = request.form
 
-    username = post_data["username"]
-    password = post_data["password"]
-    email = post_data["email"]
+    podcast_name = post_data["podcast_name"]
+    rating = post_data["rating"]
+    genre = post_data["genre"]
 
-    sql = """INSERT INTO users (username, password, email)
+    sql = """INSERT INTO podcasts (podcast_name, rating, genre)
                 VALUES (%s,%s,%s)
                 RETURNING id;"""
 
-    curr.execute(sql, (username, password, email))
+    curr.execute(sql, (podcast_name, rating, genre))
     return_id = curr.fetchone()[0]
     conn.commit()
     curr.close()
     conn.close()
     return {
-        "created_user_id": return_id
+        "created_podcast_id": return_id
     }
+
+
+@app.route('/create_episode', methods=['POST'])
+def create_episode():
+    curr = conn.cursor()
+    post_data = request.form
+
+    episode_name = post_data["episode_name"]
+    duration = post_data["duration"]
+    upload_date = post_data["upload_date"]
+    location_url = post_data["location_url"]
+
+    sql = """INSERT INTO episodes (episode_name, duration, upload_date, location_url)
+                VALUES (%s,%s,%s,%s)
+                RETURNING id;"""
+
+    curr.execute(sql, (episode_name, duration, upload_date, location_url))
+    return_id = curr.fetchone()[0]
+    conn.commit()
+    curr.close()
+    conn.close()
+    return {
+        "created_episode_id": return_id
+    }
+
 if __name__ == '__main__':
     app.run()
 
